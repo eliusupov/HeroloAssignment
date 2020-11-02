f48cc862b488ac04da1db9a511477897cd14b3d5
Eli <eli@codex.co.il>
2020-11-02 22:37:11 +0200

    .

--- server/app.js
+++ server/app.js
@@ -32,7 +32,11 @@ app.use(apiErrorHandler);
 
 app.use(express.static(path.join(__dirname, '..dist')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+});
+
+app.get('/*', (req, res) => {
 	res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
